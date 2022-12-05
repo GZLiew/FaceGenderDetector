5ac0827d50e770e08a8d9d588a75351c0abf66bb
gzliew <guozhangliew@gmail.com>
2022-12-05 20:11:24 +0800

    update model and image resizer

--- app/screens/CameraScreen.tsx
+++ app/screens/CameraScreen.tsx
@@ -1,25 +1,46 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { Camera, CameraType, FaceDetectionResult } from "expo-camera"
 import * as FaceDetector from "expo-face-detector"
 import * as FileSystem from "expo-file-system"
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, View, Text } from "react-native"
 import * as tf from "@tensorflow/tfjs"
 import { bundleResourceIO, decodeJpeg } from "@tensorflow/tfjs-react-native"
-const modelJSON = require("../tfmodels/model.json")
-const modelWeights = require("../tfmodels/group1-shard1of1.bin")
+const modelJSON = require("../tfmodels/face/model.json")
+const modelWeight1 = require("../tfmodels/face/group1-shard1of10.bin")
+const modelWeight2 = require("../tfmodels/face/group1-shard2of10.bin")
+const modelWeight3 = require("../tfmodels/face/group1-shard3of10.bin")
+const modelWeight4 = require("../tfmodels/face/group1-shard4of10.bin")
+const modelWeight5 = require("../tfmodels/face/group1-shard5of10.bin")
+const modelWeight6 = require("../tfmodels/face/group1-shard6of10.bin")
+const modelWeight7 = require("../tfmodels/face/group1-shard7of10.bin")
+const modelWeight8 = require("../tfmodels/face/group1-shard8of10.bin")
+const modelWeight9 = require("../tfmodels/face/group1-shard9of10.bin")
+const modelWeight10 = require("../tfmodels/face/group1-shard10of10.bin")
+
+const modelWeights = [
+  modelWeight1,
+  modelWeight2,
+  modelWeight3,
+  modelWeight4,
+  modelWeight5,
+  modelWeight6,
+  modelWeight7,
+  modelWeight8,
+  modelWeight9,
+  modelWeight10,
+]
 
 const loadModel = async () => {
   //.ts: const loadModel = async ():Promise<void|tf.LayersModel>=>{
-  console.log(modelJSON)
-  await tf.ready()
-  const model = await tf
-    .loadLayersModel(bundleResourceIO(modelJSON, modelWeights))
-    .then((res) => console.log("model loaded", res))
-    .catch((e) => {
-      console.log("[LOADING ERROR] info:", e)
-      console.trace()
-    })
-  return model
+  try {
+    await tf.ready()
+    const model = await tf.loadLayersModel(bundleResourceIO(modelJSON, modelWeights))
+    console.log("model loaded")
+    return model
+  } catch (e) {
+    console.log("[LOADING ERROR] info:", e)
+    return null
+  }
 }
 
 const transformImageToTensor = async (uri: string) => {
@@ -31,13 +52,13 @@ const transformImageToTensor = async (uri: string) => {
   const imgBuffer = tf.util.encodeString(img64, "base64").buffer
   const raw = new Uint8Array(imgBuffer)
   let imgTensor = decodeJpeg(raw)
-  const scalar = tf.scalar(255)
+  // const scalar = tf.scalar(255)
   //resize the image
-  imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [300, 300])
+  imgTensor = tf.image.resizeNearestNeighbor(imgTensor, [80, 80])
   //normalize; if a normalization layer is in the model, this step can be skipped
-  const tensorScaled = imgTensor.div(scalar)
-  //final shape of the rensor
-  const img = tf.reshape(tensorScaled, [1, 300, 300, 3])
+  // const tensorScaled = imgTensor.div(scalar)
+  //final shape of the tensor
+  const img = tf.image.resizeBilinear(imgTensor, [80, 80], false)
   return img
 }
 
@@ -51,9 +72,7 @@ const makePredictions = async (batch: any, model: any, imagesTensor: any) => {
   return pred
 }
 
-export const getPredictions = async (image: any) => {
-  await tf.ready()
-  const model = (await loadModel()) as tf.LayersModel
+export const getPredictions = async (image: any, model: tf.LayersModel) => {
   const tensor_image = await transformImageToTensor(image)
   const predictions = await makePredictions(1, model, tensor_image)
   return predictions
@@ -67,11 +86,25 @@ const styles = StyleSheet.create({
 })
 
 export const CameraScreen = () => {
+  const [cameraLoaded, setCameraLoaded] = useState(false)
   const cameraRef = React.useRef<Camera>()
+  const tfModelRef = React.useRef<tf.LayersModel>(null)
   const detectedAgeAndGender = React.useRef(false)
 
   const [status] = Camera.useCameraPermissions()
   Camera.useMicrophonePermissions()
+
+  useEffect(() => {
+    setCameraLoaded(false)
+    tf.ready()
+      .then(() => {
+        return loadModel()
+      })
+      .then((model) => {
+        tfModelRef.current = model
+        setCameraLoaded(true)
+      })
+  }, [])
 
   const handleFacesDetected: any = async ({ faces }: FaceDetectionResult) => {
     console.log(faces)
@@ -83,11 +116,14 @@ export const CameraScreen = () => {
 
     let imageUri: string = null
     try {
-      // const image = await cameraRef.current.takePictureAsync()
-      // imageUri = image.uri
-      // console.log(image.uri)
+      const image = await cameraRef.current.takePictureAsync()
+      imageUri = image.uri
+      if (!imageUri) {
+        return
+      }
       detectedAgeAndGender.current = true
-      await loadModel()
+      const predictions = await getPredictions(imageUri, tfModelRef.current)
+      console.log(JSON.stringify(predictions, null, 2))
       // getPredictions(imageUri)
     } catch (error) {
       console.log(error)
@@ -98,7 +134,8 @@ export const CameraScreen = () => {
 
   return (
     <View style={[styles.root]}>
-      {status?.granted && (
+      {!cameraLoaded && <Text>Loading camera...</Text>}
+      {cameraLoaded && status?.granted && (
         <Camera
           ref={cameraRef}
           onFacesDetected={handleFacesDetected}
